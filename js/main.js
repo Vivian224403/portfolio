59a4accfd6eeaccb8cf1653d3766a07301f9f4d2
kennyrocker <kennyrocker82@gmail.com>
2020-04-15 18:28:47 -0400

    update home page, remove abut page, add secondary nav menu

--- js/main.js
+++ js/main.js
@@ -3,34 +3,43 @@
     "use strict";
     
     // constant
-    var LINK_ACTIVE_CLASS = 'active';
-    var LINK_INACTIVE_CLASS = '';
+    var LINK_ACTIVE_CLASS = 'nav-link active';
+    var LINK_INACTIVE_CLASS = 'nav-link';
     var PAGE_ACTIVE_CLASS = 'page active';
     var PAGE_INACTIVE_CLASS = 'page';
     var HEADER_CLASS = 'header';
     var SHRINK_HEADER_CLASS = 'header shrink';
     var PREFIX_PAGE = 'page-';
     var HEADER_HEIGHT = 150; //px
+
     // flag
     var headerHasShrink = false;
 
     // cache elements
     var navLink = document.querySelectorAll('#nav .nav-link');
-    var navLinkA = document.querySelectorAll('#nav .nav-link a');
+    var subNavLink = document.querySelectorAll('.nav-dropdown a');
     var pages = document.querySelectorAll('#content .page');
     var header = document.getElementById('header');
 
-    
 
     // functions
     var resetNavLinks = function() {
-        for(var i = 0; i < navLinkA.length; i++) {
-            navLinkA[i].className = LINK_INACTIVE_CLASS;
+        for(var i = 0; i < navLink.length; i++) {
+            navLink[i].className = LINK_INACTIVE_CLASS;
+        }
+        for(var j=0; j< subNavLink.length; j++) {
+            subNavLink[j].className = '';
         }
     };
 
     var setNavLinkActive = function(navLinkId) {
-        document.getElementById(navLinkId).className = LINK_ACTIVE_CLASS;
+        var target = document.getElementById(navLinkId);
+        if (target.parentElement.className === LINK_INACTIVE_CLASS) {
+            target.parentElement.className = LINK_ACTIVE_CLASS;
+        } else if (target.parentElement.parentElement.parentElement.className === LINK_INACTIVE_CLASS) {
+            target.className = "active";
+            target.parentElement.parentElement.parentElement.className = LINK_ACTIVE_CLASS;
+        }
     };
 
     var hideAllPages = function() {
@@ -41,7 +50,6 @@
 
     var revealPage = function(pageId) {
         var id = PREFIX_PAGE + pageId;
-        console.log(id);
         document.getElementById(id).className = PAGE_ACTIVE_CLASS;
     };
 
